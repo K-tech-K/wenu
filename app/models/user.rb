bd7d9d8a2a8f9c3462a6e273da34060667387026
KK <kentech.1x1x3@gmail.com>
2021-08-22 05:01:14 +0000

    drafted user.rb & related routing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,19 @@
 class User < ApplicationRecord
+    # Userインスタンスを保存する前にemailの文字列を小文字へ変換
+    before_save {self.email.downcase! }
+    
+  #バリデーション
+    # name
+    validates :name, presence: true, length: { maximum: 50 }
+    
+    # shop
+    validates :shop, presence: true, length: { maximum: 100 }
+    
+    # email
+    validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    uniqueness: { case_sensitive: false }
+    
+    # password_digestセキュリティ用コード
+    has_secure_password
 end
