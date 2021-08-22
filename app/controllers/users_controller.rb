4fb6c42a068593bb7ebd1c800952182445db3c66
KK <kentech.1x1x3@gmail.com>
2021-08-22 08:11:27 +0000

    build User model,controller,views & login function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, except: [:new]
   
   def show
     @user = User.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  get 'toppages/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   # root
-  root to: "users#show"
+  root to: "toppages#index"
+  
+  # ログイン用ルーティング
+   get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
   # Userモデルルーティング
   get "signup", to: "users#new"
